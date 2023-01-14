5721f1278fe86349f0f88c158e9a69eca23954c6
rianphlox <skellybrownston@gmail.com>
2023-01-14 20:24:24 +0100

    commit

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 
     $conn = new mysqli('localhost', 'root', '') or die("Failed to connect to MYSQLi" . $conn->connect_error);
+    // $sql = "CREATE DATABASE IF NOT EXISTS hmo";
+    // $conn->query($sql);
+    // $r_sql = file_get_contents('hmo.sql');
+    // $conn->query($sql);
+    // $conn->select_db('hmo');
+    // $conn->multi_query($r_sql);
 
-    if (!$conn->select_db('hmo')) {
-        $r_sql = file_get_contents('hmo.sql');
+    // header('location: zh\user.php');
+    $sql = "SHOW DATABASES LIKE 'hmo';";
+    $result = $conn->query($sql);
+    if (!$result->num_rows) {
         $sql = "CREATE DATABASE IF NOT EXISTS hmo";
+        $conn->query($sql);
+        $r_sql = file_get_contents('hmo.sql');
         $conn->query($sql);
         $conn->select_db('hmo');
         $conn->multi_query($r_sql);
+        header('location: zh\user.php');
+    } else {
+        header('location: zh\user.php');
     }
-    header('location: zh\user.php');
